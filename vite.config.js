a51948b37ee4e1c9cfd72c0381315cafe6c67b5b
Lakshay_Pratap_Singh <pratapsinghlakshay8@gmail.com>
2025-03-19 16:26:03 +0530

    added a line in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/landingPage/", // <-- Add your repo name here
   plugins: [react()],
-})
+});
